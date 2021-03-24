8fc4a2b679d7c0fe6d90392f8ac444f73522f594
jcasanella <jcasanella@mimecast>
2021-03-24 20:20:06 +0000

    First exercises

---
+++ application_structure/main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+var x int = 100
+
+const y = 3600
+
+func main() {
+	var a int = 7
+	var b float64 = 3.5
+	const c int = 10
+
+	fmt.Println("Hello test")
+	fmt.Println(a, b, c)
+}
