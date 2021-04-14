d36a6e647d98717304c180823f6cf0792b0115f8
Jordi Casanella <jordikiter@hotmail.es>
2021-04-14 23:57:20 +0100

    Exercises basic

--- 2_basics/exercises/main.go
+++ 2_basics/exercises/main.go
@@ -1,6 +1,10 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+
+)
 
 var version = "3.1"
 
@@ -13,8 +17,16 @@ func main() {
 	)
 
 	x, y, z := 20, 15.5, "Gopher!"
+	score := []int{10, 20, 30}
+	fmt.Printf("%d %f %s %#v\n", x, y, z, score)
+	fmt.Printf("%q\n", z)
+	fmt.Printf("%v %v %v %v\n", x, y, z, score)
+	fmt.Printf("%T %T\n", y, score)
 
 	_, _, _, _, _, _, _ = a, b, c, d, x, y, z
+
+	const x2 float64 = 1.422349587101
+	fmt.Printf("%.4f\n", x2)
 
 	var a1 float64 = 7.1
 	x1, y1 := true, 3.7
@@ -43,4 +55,51 @@ func main() {
 	)
 
 	fmt.Println(Jun, Jul, Aug)
+
+	shape := "circle"
+	radius := 3.2
+	const pi2 float64 = 3.14159
+	circumference := 2 * pi2 * radius
+
+	fmt.Printf("Shape: %q\n", shape)
+	fmt.Printf("Circle's circumference with radius %f is %f\n", radius, circumference)
+
+	var i int = 3
+	var f float64 = 3.2
+
+	fmt.Printf("%f %d\n", float64(i), int(f))
+
+	var i2 = 3
+	var f1 = 3.2
+	var s1, s2 = "3.14", "5"
+
+	s1 = strconv.Itoa(i2)
+	s3, _ := strconv.Atoi(s2)
+	s4 := strconv.FormatFloat(f1, 'E', -1, 64)
+	s5, _ := strconv.ParseFloat(s1, 64)
+	fmt.Printf("%T %s\n", s1, s1)
+	fmt.Printf("%T %d\n", s3, s3)
+	fmt.Printf("%T %s\n", s4, s4)
+	fmt.Printf("%T %f\n", s5, s5)
+
+	x3, y3 := 4, 5.1
+	z3 := float64(x3) * y3
+	fmt.Println(z3)
+
+	a3 := 5
+	b3 := 6.2 * float64(a3)
+	fmt.Println(b3)
+
+	type duration int
+	var hour duration
+
+	fmt.Printf("%v %T\n", hour, hour)
+
+	hour = 3600
+	fmt.Printf("%v %T\n", hour, hour)
+
+	minute := 60
+	fmt.Println(int(hour) != minute)
+	fmt.Println(hour != duration(minute))
+
 }
