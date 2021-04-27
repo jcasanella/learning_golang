ec6b10cf01a01b12a73a6a43dbd71e75ba5ca06f
Jordi Casanella <jordikiter@hotmail.es>
2021-04-27 23:38:31 +0100

    Loop

---
+++ 3_flow_control/loops/main.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main() {
+	for i := 0; i < 10; i++ {
+		fmt.Println(i)
+	}
+
+	j := 10
+	for j > 0 {
+		fmt.Println(j)
+		j--
+	}
+
+	/*
+		infinite loop
+		for {
+
+		}
+	*/
+}
