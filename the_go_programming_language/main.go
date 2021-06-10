bc341109a4ec8ecf911e1b7474bac1399e7bba7a
jcasanella <jordikiter@hotmail.es>
2021-06-10 23:53:46 +0100

    Exercise kelvin converter

--- the_go_programming_language/main.go
+++ the_go_programming_language/main.go
@@ -7,11 +7,63 @@ import (
 )
 
 func main() {
-	fmt.Printf("%v\n", tempconv.BoilingC)
-	fmt.Printf("%g\n", tempconv.BoilingC-tempconv.FreezingC)
+	// Fah to Kel
+	fmt.Println("Fahrenheit to Kelvin")
+	fmt.Printf("%g\n", tempconv.FToK(-30))
+	fmt.Printf("%g\n", tempconv.FToK(-20))
+	fmt.Printf("%g\n", tempconv.FToK(-10))
+	fmt.Printf("%g\n", tempconv.FToK(0))
+	fmt.Printf("%g\n", tempconv.FToK(10))
+	fmt.Printf("%g\n", tempconv.FToK(20))
+	fmt.Printf("%g\n", tempconv.FToK(30))
 
-	boilingF := tempconv.CToF(tempconv.BoilingC)
-	fmt.Printf("%g\n", boilingF-tempconv.CToF(tempconv.FreezingC))
+	// Fah to Cel
+	fmt.Println("Fahrenheit to Celsius")
+	fmt.Printf("%g\n", tempconv.FToC(-30))
+	fmt.Printf("%g\n", tempconv.FToC(-20))
+	fmt.Printf("%g\n", tempconv.FToC(-10))
+	fmt.Printf("%g\n", tempconv.FToC(0))
+	fmt.Printf("%g\n", tempconv.FToC(10))
+	fmt.Printf("%g\n", tempconv.FToC(20))
+	fmt.Printf("%g\n", tempconv.FToC(30))
 
-	fmt.Printf("%s", tempconv.FreezingC)
+	// Cel to Kel
+	fmt.Println("Celcius to Kelvin")
+	fmt.Printf("%g\n", tempconv.CToK(-30))
+	fmt.Printf("%g\n", tempconv.CToK(-20))
+	fmt.Printf("%g\n", tempconv.CToK(-10))
+	fmt.Printf("%g\n", tempconv.CToK(0))
+	fmt.Printf("%g\n", tempconv.CToK(10))
+	fmt.Printf("%g\n", tempconv.CToK(20))
+	fmt.Printf("%g\n", tempconv.CToK(30))
+
+	// Celc to Fah
+	fmt.Println("Celcius to Fahrenheit")
+	fmt.Printf("%g\n", tempconv.CToF(-30))
+	fmt.Printf("%g\n", tempconv.CToF(-20))
+	fmt.Printf("%g\n", tempconv.CToF(-10))
+	fmt.Printf("%g\n", tempconv.CToF(0))
+	fmt.Printf("%g\n", tempconv.CToF(10))
+	fmt.Printf("%g\n", tempconv.CToF(20))
+	fmt.Printf("%g\n", tempconv.CToF(30))
+
+	// Kel to Fah
+	fmt.Println("Kelvin to Fahrenheit")
+	fmt.Printf("%g\n", tempconv.KToF(-30))
+	fmt.Printf("%g\n", tempconv.KToF(-20))
+	fmt.Printf("%g\n", tempconv.KToF(-10))
+	fmt.Printf("%g\n", tempconv.KToF(0))
+	fmt.Printf("%g\n", tempconv.KToF(10))
+	fmt.Printf("%g\n", tempconv.KToF(20))
+	fmt.Printf("%g\n", tempconv.KToF(30))
+
+	// Kel to Cel
+	fmt.Println("Kelvin to Celsius")
+	fmt.Printf("%g\n", tempconv.KToC(-30))
+	fmt.Printf("%g\n", tempconv.KToC(-20))
+	fmt.Printf("%g\n", tempconv.KToC(-10))
+	fmt.Printf("%g\n", tempconv.KToC(0))
+	fmt.Printf("%g\n", tempconv.KToC(10))
+	fmt.Printf("%g\n", tempconv.KToC(20))
+	fmt.Printf("%g\n", tempconv.KToC(30))
 }
--- the_go_programming_language/tempconv/tempconv0.go
+++ the_go_programming_language/tempconv/tempconv0.go
@@ -4,19 +4,37 @@ import "fmt"
 
 type Celsius float64
 type Farenheit float64
+type Kelvin float64
 
 const (
 	AbsoluteZeroC Celsius = -273.15
 	FreezingC     Celsius = 0
 	BoilingC      Celsius = 100
+	KelvinFah     Kelvin  = 459.67
 )
+
+func FToK(f Farenheit) Kelvin {
+	return Kelvin((f + 459.67) * 5 / 9)
+}
 
 func CToF(c Celsius) Farenheit {
 	return Farenheit(c*9/5 + 32)
 }
 
+func CToK(c Celsius) Kelvin {
+	return Kelvin(c - AbsoluteZeroC)
+}
+
+func KToF(k Kelvin) Farenheit {
+	return Farenheit(k*9/5 - KelvinFah)
+}
+
 func FToC(f Farenheit) Celsius {
 	return Celsius((f - 32) * 5 / 9)
+}
+
+func KToC(k Kelvin) Celsius {
+	return Celsius(k) + AbsoluteZeroC
 }
 
 func (c Celsius) String() string {
@@ -25,4 +43,8 @@ func (c Celsius) String() string {
 
 func (f Farenheit) String() string {
 	return fmt.Sprintf("%g F", f)
+}
+
+func (k Kelvin) String() string {
+	return fmt.Sprintf("%g F", k)
 }
