a6ff17e987cae9bd17f6559ace9e4d65507944c5
jcasanella <jordikiter@hotmail.es>
2021-06-19 23:18:03 +0100

    Process multipages

--- web_app/server.go
+++ web_app/server.go
@@ -13,8 +13,21 @@ func main() {
 	// Response writer used to print the html
 	// http.Request contains all the info about this Http request
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello you've requested from: %s\n", r.URL.Path)
+		fmt.Fprintf(w, "<h1>Hello you've requested from: %s</h1>\n", r.URL.Path)
+		if name := r.URL.Query().Get("name"); name != "" {
+			fmt.Fprintf(w, "What's up %s", name)
+		}
 	})
+
+	http.HandleFunc("/test", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, "<h1>This is a test page</h1>")
+		if name := r.URL.Query().Get("name"); name != "" {
+			fmt.Fprintf(w, "Name is: <strong>%s</strong>", name)
+		}
+	})
+
+	fs := http.FileServer(http.Dir("static/"))
+	http.Handle("/static/", http.StripPrefix("/static/", fs))
 
 	// To accept connections, a server needs to listen on a port to pass connections to request handler
 	log.Fatal(http.ListenAndServe(":80", nil))
