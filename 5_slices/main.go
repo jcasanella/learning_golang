743ad0edee85384346c59347c3cabae36a4c8ae7
jcasanella <jordikiter@hotmail.es>
2021-06-20 18:08:04 +0100

    More slice exercises

--- 5_slices/main.go
+++ 5_slices/main.go
@@ -67,4 +67,27 @@ func main() {
 	} else {
 		fmt.Println("Slices are not ")
 	}
+
+	// declaring [5]int array
+	ab := [5]int{1, 2, 3, 4, 5}
+
+	// slicing array, returns an array
+	bb := ab[1:3]
+	fmt.Printf("Type: %T, value: %#v\n", bb, bb)
+
+	// declaring a slice
+	s1 := []int{1, 2, 3, 4, 5, 6}
+	s2 := s1[1:3]
+	fmt.Println(s2)
+
+	fmt.Println(s1[2:])       // 3,4,5,6
+	fmt.Println(s1[:3])       // 1,2,3
+	fmt.Println(s1[:])        // 1,2,3,4,5,6
+	fmt.Println(s1[:len(s1)]) // 1,2,3,4,5,6
+
+	s1 = append(s1[:4], 100) // adds 100 after index 4 (excluded)
+	fmt.Println(s1)
+
+	s1 = append(s1[:4], 200)
+	fmt.Println(s1)
 }
