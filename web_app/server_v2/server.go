642af34d9bf502dcc64a189e5c92800fb208ca98
jcasanella <jordikiter@hotmail.es>
2021-06-22 23:39:52 +0100

    Improving server

---
+++ web_app/server_v2/server.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"strconv"
+
+	"github.com/gorilla/mux"
+
+)
+
+func main() {
+	r := mux.NewRouter()
+	r.HandleFunc("/books/{title}/page/{page}", func(w http.ResponseWriter, r *http.Request) {
+		vars := mux.Vars(r)
+		title := vars["title"]
+		page := vars["page"]
+
+		value, _ := strconv.Atoi(page)
+		fmt.Fprintf(w, "You have requested the book: %s on page: %d\n", title, value)
+	})
+
+	http.ListenAndServe(":80", r)
+}
