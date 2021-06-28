1692aabc8a3ac7d89c97a3fcdf3874d2c98443dc
jcasanella <jordikiter@hotmail.es>
2021-06-28 23:32:53 +0100

    Exercises slices

--- 5_slices/exercises/main.go
+++ 5_slices/exercises/main.go
@@ -65,5 +65,67 @@ func main() {
 		}
 	}
 
+	fmt.Println("Calculate sum and average")
+	sum := 0
+	for _, value := range numbers {
+		sum += value
+	}
+
 	fmt.Println(numbers)
+	average := float32(sum) / float32(len(numbers))
+	fmt.Printf("Sum: %d Average: %f\n", sum, average)
+
+	// Exercise 4
+	// Consider the following slice declaration: nums := []int{5, -1, 9, 10, 1100, 6, -1, 6}
+	// Using a slice expression and a for loop iterate over the slice ignoring the first and the last two elements.
+	// Print those elements and their sum.
+	numsA := []int{5, -1, 9, 10, 1100, 6, -1, 6}
+	numsB := numsA[1 : len(numsA)-2]
+	sumB := 0
+	for _, value := range numsB {
+		fmt.Print(" ", value)
+		sumB += value
+	}
+	fmt.Println()
+	fmt.Println(sumB)
+
+	// Exercise 5
+	// Consider the following slice declaration: friends := []string{"Marry", "John", "Paul", "Diana"}
+	// Using copy() function create a copy of the slice. Prove that the slices are not connected by modifying one slice and notice
+	// that the other slice is not modified.
+	friends := []string{"Marry", "John", "Paul", "Diana"}
+	friends2 := make([]string, len(friends))
+	copy(friends2, friends)
+	fmt.Println("Friends:", friends)
+	fmt.Println("Friends2:", friends2)
+
+	friends[0] = "Peter"
+	fmt.Println("Friends:", friends)
+	fmt.Println("Friends2:", friends2)
+
+	friends2[1] = "Joe"
+	fmt.Println("Friends:", friends)
+	fmt.Println("Friends2:", friends2)
+
+	// Exercise 6
+	// Consider the following slice declaration: friends := []string{"Marry", "John", "Paul", "Diana"}
+	// Using append() function create a copy of the slice. Prove that the slices are not connected by modifying one
+	// slice and notice that the other slice is not modified.
+	friendsA := []string{"Marry", "John", "Paul", "Diana"}
+	var friendsB []string
+	friendsB = append(friendsB, friendsA...)
+	fmt.Println(friendsB)
+	friendsB[1] = "Adrian"
+	fmt.Println(friendsA)
+	fmt.Println(friendsB)
+
+	// Exercise 7
+	// Consider the following slice declaration:
+	// years := []int{2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010}
+	// Using a slice expression and append() function create a new slice called newYears
+	// that contains the first 3 and the last 3 elements of the slice.  newYears should be []int{2000, 2001, 2002, 2008, 2009, 2010}
+	years := []int{2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010}
+	var newYears []int
+	newYears = append(years[:3], years[len(years)-3:]...)
+	fmt.Println(newYears)
 }
