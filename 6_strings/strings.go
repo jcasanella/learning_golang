61bbc5937d7806f09d6333c9b7d05f168f0766e6
jcasanella <jordikiter@hotmail.es>
2021-07-14 23:45:57 +0100

    working with runes

--- 6_strings/strings.go
+++ 6_strings/strings.go
@@ -1,6 +1,10 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"unicode/utf8"
+
+)
 
 func main() {
 	s1 := "Here we go"
@@ -27,4 +31,24 @@ func main() {
 	fmt.Println(s3 + "!!!")
 
 	fmt.Println("Element at index 0: ", s3[0])
+
+	// runes
+	// characters or rune literals are expressed in Go by enclosing them in single quotes
+	// declaring a variable of type rune (alias to int32)
+	var1 := 'a'
+	fmt.Printf("Type: %T, Value %d\n", var1, var1)
+
+	// declaring a string value that contains non-ascii characters
+	str := "ţară" // ţară means country in Romanian
+	// 't', 'a' ,'r' and 'a' are runes and each rune occupies beetween 1 and 4 bytes.
+
+	//The len() built-in function returns the no. of bytes not runes or chars.
+	fmt.Println(len(str)) // -> 6,  4 runes in the string but the length is 6
+
+	// returning the number of runes in the string
+	m := utf8.RuneCountInString(str)
+	fmt.Println(m) // => 4
+
+	// by using indexes we get the byte at that positioin, not rune.
+	fmt.Println("Byte (not rune) at position 1:", str[1]) // -> 163
 }
